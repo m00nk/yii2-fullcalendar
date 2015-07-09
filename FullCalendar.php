a9b298fab41542c0e4af607c85ace4e473fc7b2a
Dmitrij Sheremetjev <m00nk1975@gmail.com>
2015-07-09 21:59:32 +0300

    Initial

---
+++ FullCalendar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright (C) FIT-Media.com (fit-media.com), {@link http://tanitacms.net}
+ * Date: 09.07.15, Time: 21:57
+ *
+ * @author        Dmitrij "m00nk" Sheremetjev <m00nk1975@gmail.com>
+ * @package
+ */
+
+namespace m00nk\fullcalendar;
+
+use yii\helpers\Html;
+use yii\base\Widget;
+use \Yii;
+
+
+class FullCalendar extends Widget
+{
+	public function run()
+	{
+		return '<h1>IT IS ME!!!</h1>';
+	}
+}
